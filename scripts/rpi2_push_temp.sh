f3aea40895518e283dd01473c4a829dc54e75a86
rjonsson <rjonssonovik@gmail.com>
2016-11-19 17:48:36 +0100

    small mod

--- scripts/rpi2_push_temp.sh
+++ scripts/rpi2_push_temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/python ./python/rpi_readtemp.py
+/usr/bin/python ./python/rpi2_readtemp.py
 sleep 10
 temp_intake=$(<temp_intake.data)
 curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi2,sensor=intake value=$temp_intake"
