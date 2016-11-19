2ad4636a1ecfd4c9a33a7692998ab88e879c79de
rjonsson <rjonssonovik@gmail.com>
2016-11-19 16:28:39 +0000

    adding rpi 1 script

--- scripts/python/rpi2_readtemp.py
+++ scripts/python/rpi2_readtemp.py
@@ -1,23 +1,18 @@
 import os
 import glob
 import time
- 
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
- 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
 
-device1_file = '/sys/bus/w1/devices/28-0316644ee8ff/w1_slave'
-device2_file = '/sys/bus/w1/devices/28-0416637613ff/w1_slave'
- 
+device1_file = '/sys/bus/w1/devices/28-0416637b2fff/w1_slave'
+
 def read_temp_raw(rawfile):
     f = open(rawfile, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp(usefile):
     lines = read_temp_raw(usefile)
     while lines[0].strip()[-3:] != 'YES':
@@ -28,11 +23,9 @@ def read_temp(usefile):
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
-	
-temp_in=read_temp(device1_file)	
-temp_out=read_temp(device2_file)	
 
-f1 = open('temp_in.data','w')
-f2 = open('temp_out.data','w')
-f1.write(str(temp_in))
-f2.write(str(temp_out))
+temp_intake=read_temp(device1_file)
+
+f1 = open('temp_intake.data','w')
+f1.write(str(temp_intake))
+
