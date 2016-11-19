d54d20b2af79cd8a21400822644a0b39a235cb6e
rjonsson <rjonssonovik@gmail.com>
2016-11-19 23:07:56 +0100

    update for switch

--- scripts/rpi_push_temp.sh
+++ scripts/rpi_push_temp.sh
@@ -6,6 +6,9 @@ scriptdir=$(dirname $0)
 #Call python script for temperature sensor reading and read variables for each line
 read -r temp_in temp_out <<< $(/usr/bin/python $scriptdir/python/rpi_readtemp.py)
 
+read switch_temp <<< $(snmpwalk -v2c -c public 192.168.1.21 1.3.6.1.4.1.9.9.13.1.3.1.3.1006 | cut -d: -f2 | sed 's/ //')
+
 #Upload data to to InfluxDB
 curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi,sensor=inside value=$temp_in"
 curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi,sensor=outside value=$temp_out"
+curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=c3750e,sensor=onboard_temp value=$switch_temp"
