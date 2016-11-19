b69bfed4d89a9ae677a291ef5de95d4b2131bd3a
rjonsson <rjonssonovik@gmail.com>
2016-11-19 16:30:53 +0000

    modding rpi_push

--- scripts/rpi_push_temp.sh
+++ scripts/rpi_push_temp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-/usr/bin/python readtemp.py
+/usr/bin/python ./python/rpi_readtemp.py
 sleep 10
 temp_in=$(<temp_in.data)
 temp_out=$(<temp_out.data)
 curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi,sensor=inside value=$temp_in"
 curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi,sensor=outside value=$temp_out"
+rm temp_in.data
+rm temp_out.data
