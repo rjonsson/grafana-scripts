da443e0deefbfed8e5666d398f1d0674d1adb9e5
rjonsson <rjonssonovik@gmail.com>
2016-11-19 19:01:33 +0100

    Clean & Commentary

--- scripts/rpi_push_temp.sh
+++ scripts/rpi_push_temp.sh
@@ -3,22 +3,9 @@
 readlink -f "$0"
 scriptdir=$(dirname $0)
 
-#Call python script for temperature sensor reading
+#Call python script for temperature sensor reading and read variables for each line
 read -r temp_in temp_out <<< $(/usr/bin/python $scriptdir/python/rpi_readtemp.py)
-#/usr/bin/python $scriptdir/python/rpi_readtemp.py
-#sleep 10
-echo $temp_in
-echo $temp_out
-
-
-#Read files that the python script produces
-#temp_in=$(<temp_in.data)
-#temp_out=$(<temp_out.data)
 
 #Upload data to to InfluxDB
 curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi,sensor=inside value=$temp_in"
 curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi,sensor=outside value=$temp_out"
-
-#Clean up datafiles
-#rm temp_in.data
-#rm temp_out.data
