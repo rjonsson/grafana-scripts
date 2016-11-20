57225216c4b74ec4829ed4dfac09966a8060444a
rjonsson <rjonssonovik@gmail.com>
2016-11-20 21:14:43 +0100

    fixes

--- scripts/hw_push.sh
+++ scripts/hw_push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #usage ram_push <ip>
-ipInfluxDB="192.168.1.120"
-
 
 read hostname <<< $(snmpget -v2c -c public $1 -m SNMPv2-MIB sysName.0 | cut -d: -f4 | sed 's/ //')
 read -a memArray <<< $(snmpwalk -v2c -c public $1 -m UCD-SNMP-MIB memory | awk '{gsub("UCD-SNMP-MIB::", "");print $1 $4}' | sed 's/.0/:/' | grep -v Swap | sed -n '4,8p')
@@ -11,11 +9,11 @@ read -a cpuArray <<< $(snmptable -v2c -c public $1 -m UCD-SNMP-MIB laTable | sed
 for index in "${!memArray[@]}"
 do
 	IFS=':' read a b <<< $(echo "${memArray[index]}")
-	curl -i -XPOST 'http://$ipInfluxDB:8086/write?db=home&precision=s' --data-binary "hw,host=$hostname,type=mem,subtype=$a value=$b"
+	curl -i -XPOST 'http://192.168.1.120:8086/write?db=home&precision=s' --data-binary "hw,host=$hostname,type=mem,subtype=$a value=$b"
 done
 
 for index in "${!cpuArray[@]}"
 do
 	IFS=':' read a b <<< $(echo "${cpuArray[index]}")
-	curl -i -XPOST 'http://$ipInfluxDB:8086/write?db=home&precision=s' --data-binary "hw,host=$hostname,type=cpu,subtype=$a value=$b"
+	curl -i -XPOST 'http://192.168.1.120:8086/write?db=home&precision=s' --data-binary "hw,host=$hostname,type=cpu,subtype=$a value=$b"
 done
