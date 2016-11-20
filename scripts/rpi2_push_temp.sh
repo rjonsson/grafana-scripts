6c798d06b12055b78c77281110d4a1a969133f45
rjonsson <rjonssonovik@gmail.com>
2016-11-20 16:12:16 +0100

    Cleanup

--- scripts/rpi2_push_temp.sh
+++ scripts/rpi2_push_temp.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#Get scriptdir for external script call
-readlink -f "$0"
-scriptdir=$(dirname $0)
-
 #Call python script for temperature sensor reading and read variable
 read temp_intake <<< $(/usr/bin/python $scriptdir/python/rpi2_readtemp.py)
 
 #Upload data to to InfluxDB
-curl -i -XPOST 'http://192.168.1.120:8086/write?db=home' --data-binary "temp,host=rpi2,sensor=intake value=$temp_intake"
+curl -i -XPOST 'http://192.168.1.120:8086/write?db=home&precision=s' --data-binary "temp,host=rpi2,sensor=intake value=$temp_intake"
